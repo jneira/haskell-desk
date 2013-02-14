3a707e622e68ef05dfff08ad00614428da947d79
jneira <atreyu.bbb@gmail.com>
2013-02-14 21:04:28 +0100

    changes in data structures

--- algo/Dijkstra.hs
+++ algo/Dijkstra.hs
@@ -1,5 +1,4 @@
 module Main where
-import Data.Graph
 import Data.Array.Diff
 import Data.Heap 
 import Data.Set hiding (map,toList,fromList,fromAscList,
@@ -10,71 +9,84 @@ import Data.Ord (comparing)
 import System.Environment
 import System.Time (getClockTime, diffClockTimes) 
 
-type Vertices=DiffUArray Vertex Vertex
-type MinPaths=(Vertices,Vertices)
-type GraphWithDists=Table [(Vertex,Int)] 
+type Vertex=Int
+type Distance=Int
+type Path=[Vertex]
+type PrevsVertices=DiffUArray Vertex Vertex
+type Distances=DiffUArray Vertex Distance
+type MinPaths=(Distances,PrevsVertices)
+type WeightedGraphAL=Array Vertex [(Vertex,Int)] 
+type WeightedGraphM=Array (Vertex,Vertex) Distance
 
 inf :: Int
 inf= maxBound `div` 2
 
-solve :: GraphWithDists -> Vertex -> MinPaths  
-solve g v=step g (initMinPaths (Set.size q) v) q
-  where q= Set.fromList $ vertices $ fmap (map fst) g 
+solve :: Int -> [((Vertex,Vertex),Distance)] -> Vertex -> MinPaths  
+solve n g v=step g' (initMinPaths n v) q
+  where g'=buildGraphM n g  
+        q= Set.fromList $ [1..n]
+
+buildGraphM :: Int -> [((Vertex,Vertex),Distance)] -> WeightedGraphM
+buildGraphM n items=accum (const id) init items 
+  where init=listArray ((1,1),(n,n)) $ repeat inf
 
 initMinPaths :: Int -> Vertex -> MinPaths
 initMinPaths n v= (dist//[(v,0)],previous)
   where dist=listArray (1,n) $ repeat inf   
         previous=listArray (1,n) $ repeat v
 
-step :: GraphWithDists -> MinPaths -> Set Vertex -> MinPaths 
-step g mps@(dists,previous) q
+step :: WeightedGraphM -> MinPaths -> Set Vertex -> MinPaths 
+step g mps@(dists,previous) q -- O(n*(2n+logn)) ~> O(n^2)
   | Set.size q < 2 = mps
-  | otherwise = step g mps' q' -- O(n)
+  | otherwise = step g mps' q' 
   where u = minVertexByDist dists q -- O(n) 
         q'= delete u q -- O(logn)
-        mps'=foldr (updateDist u) mps (g!u) -- O(n)
+        mps'=foldr (updateDist g u) mps [1..snd.snd $ bounds g] -- O(n)
 
-minVertexByDist ::  Vertices -> Set Vertex -> Vertex
+minVertexByDist ::  Distances -> Set Vertex -> Vertex
 minVertexByDist dist q = fold min' h  q   
   where h=head.Set.toList $ q 
         min' n p=if (dist!p)<(dist!n) then p else n
 
-updateDist :: Vertex -> (Vertex,Int) -> 
+updateDist :: WeightedGraphM -> Vertex -> Vertex -> 
               MinPaths -> MinPaths 
-updateDist u (v,d) ds@(dists,prevs)
+updateDist g u v ds@(dists,prevs)
   | dists!v > newDist = (dists//[(v,newDist)],prevs//[(v,u)]) 
   | otherwise = ds
-  where newDist=(dists!u) + d
+  where d=g!(u,v) 
+        newDist=(dists!u) + d
 
-gwd  :: GraphWithDists
-gwd=listArray (1,7) 
-    [[(2,10),(5,6)],[(3,5),(4,2)],[(4,4)],
-     [(1,5)],[(6,9),(7,1)],[(4,3),(7,8)],[]]
-
-minPath :: Vertices -> Vertex -> Vertex -> [Vertex] 
+minPath :: PrevsVertices -> Vertex -> Vertex -> [Vertex] 
 minPath vs v w | w == v = []
                | prev == v = [v]
                | otherwise = prev:minPath vs v prev  
   where prev=vs!w
 
-type DistsHeap=MinPrioHeap Int Vertex
+type DistsHeap=MinPrioHeap Distance Vertex
 
-solveWithHeap g v=solve' g v
+solveWithHeap :: Int -> [((Vertex,Vertex),Distance)] -> Vertex -> MinPaths
+solveWithHeap n g v=solve' g' v
+  where g'=buildGraphAL n g 
 
-solve' :: GraphWithDists -> Vertex -> MinPaths  
-solve' g v= step' g (dists,prevs) hp
+buildGraphAL :: Int -> [((Vertex,Vertex),Distance)] -> WeightedGraphAL
+buildGraphAL n items=accumArray (flip (:)) [] (1,n) items' 
+  where items'=map (\((x,y),z)->(x,(y,z))) items
+        
+solve' :: WeightedGraphAL -> Vertex -> MinPaths  
+solve' g v= step' g (dists,prevs) hp -- O(n+aloga)??
   where n= rangeSize $ bounds g 
-        hp= singleton (1,v)
-        (dists,prevs)=initMinPaths n v
+        hp= singleton (1,v) 
+        (dists,prevs)=initMinPaths n v -- O(n)
 
-step' :: GraphWithDists -> MinPaths -> DistsHeap -> MinPaths 
-step' g mps@(dists,prevs) hp
+step' :: WeightedGraphAL -> MinPaths -> DistsHeap -> MinPaths 
+step' g mps@(dists,prevs) hp -- O(a*log a)??
   | size hp == 0 = mps
-  | otherwise = step' g mps' hp'' -- O(n)
-  where Just((d,u),hp') = view hp -- O(logn) 
-        (hp'',mps')=foldr (updateDist' u) (hp',(dists,prevs)) (g!u) -- O(alogn)
+  | otherwise = step' g mps' hp'' --O(a)
+  where Just((d,u),hp') = view hp  -- O(loga)
+        (hp'',mps')=foldr (updateDist' u) --O(loga) 
+                    (hp',(dists,prevs)) (g!u)
 
-updateDist' :: Vertex -> (Vertex,Int) -> 
+updateDist' :: Vertex -> (Vertex,Distance) -> 
               (DistsHeap,MinPaths) -> (DistsHeap,MinPaths) 
 updateDist' u (v,d) ds@(hp,(dists,prevs))
   | dists!v > newDist = (hp',(dists',prevs//[(v,u)])) 
@@ -83,31 +95,32 @@ updateDist' u (v,d) ds@(hp,(dists,prevs))
         dists'=dists//[(v,newDist)]
         hp'=insert (newDist,v) hp 
 
+gwd  :: [((Vertex,Vertex),Distance)]
+gwd=[((1,2),10),((1,5),6),((2,3),5),((2,4),2),((3,4),4),((3,1),2),
+     ((4,1),5),((5,6),9),((5,7),1),((6,4),3),((6,7),8)]
+
 -- Actions
 readWords :: Read a => String -> [a]
 readWords s= ws
  where ws=map read $ words s
 
-readItem :: String -> (Vertex,(Vertex,Int))
-readItem str=(i,(j,abs c))
+readItem :: String -> ((Vertex,Vertex),Distance)
+readItem str=((i,j),abs c) -- No negative weights allowed
  where [i,j,c]=readWords str
 
-readData :: FilePath -> IO (Int,[(Vertex,(Vertex,Int))])
+readData :: FilePath -> IO (Int,[((Vertex,Vertex),Distance)])
 readData file= do
  (h:s) <-lines `fmap` readFile file
  let [n,_]=readWords h
      items=map readItem s
  return (n,items)
 
-buildGraph :: Int -> [(Vertex,(Vertex,Int))] -> GraphWithDists
-buildGraph n items=accumArray (flip (:)) [] (1,n) items 
 
-sol :: FilePath -> Vertex -> Vertex -> [String] -> IO (Int,[Vertex])
+sol :: FilePath -> Vertex -> Vertex -> [String] -> IO (Distance,[Vertex])
 sol fn v w opts= do
   (n,edges) <- readData fn
-  let g=buildGraph n edges
-      (dists,prevs)=if elem "-h" opts then solveWithHeap g v 
-                    else solve g v 
+  let (dists,prevs)=if elem "-h" opts then solveWithHeap n edges v 
+                    else solve n edges v 
   return $ (dists!w,minPath prevs v w)
 
 main=do
--- algo/TaskManager.hs
+++ algo/TaskManager.hs
@@ -1,7 +1,19 @@
 module Main where
+import Data.List (mapAccumL)
 
-type Task=(Int,Int)
+type Bonus=Int
+type Time=Int
+type Task=(Bonus,Time)
+
 
 viable :: [Task] -> Bool 
 viable=undefined
+
+schedule :: [Task] -> (Time,[Time]) 
+schedule = mapAccumL addTime 0
+  where addTime t (_,tt)=(t+tt,tt-t)
+
+allOnTime :: [Task] -> Bool 
+allOnTime=(all (>0)).snd.schedule 
+--  where finishedBefore p n=
 
